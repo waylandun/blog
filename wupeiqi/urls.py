1baf1bbf004c516f1d43141cfa79d3af9f85bdaf
AMD 7700x <7700x@wayland.com>
2023-12-20 21:33:48 +0800

    新增与修改部门

--- wupeiqi/urls.py
+++ wupeiqi/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employee/', views.employee),
-    path('department/', views.department),
+    path('employee/', views.employee_list),
+    path('depart/list/', views.department_list, name="department_list"),
+    path('depart/add/', views.add_department, name="add_department"),
+    path('depart/update/<int:depart_id>/', views.update_department, name="update_department"),
 ]
--- wupeiqi/views.py
+++ wupeiqi/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 from .models import Department, UserInfo
 
 
 # Create your views here. 20221214
-def employee(request):
-    return render(request,'employee.html')
+def employee_list(request):
+    return render(request, 'employee.html')
 
 
-def department(request):
+def department_list(request):
     departs = Department.objects.all()
     meta = {}
     meta["departs"] = departs
     return render(request, 'department.html', meta)
+
+
+def add_department(request):
+    if request.POST:
+        title = request.POST.get('title', "")
+        dh = request.POST.get('dh', '')
+        Department.objects.create(title=title, dh=dh)
+        return redirect(to="department_list")
+    return render(request, 'add_depart.html')
+
+
+def update_department(request, depart_id):
+    # depart_id = request.GET.get('id', '')
+    print()
+    depart = Department.objects.filter(id=int(depart_id)).first()
+    if request.POST:
+        title = request.POST.get('title', "")
+        dh = request.POST.get('dh', '')
+        Department.objects.filter(id=depart_id).update(title=title, dh=dh)
+        return redirect(to="department_list")
+    return render(request, 'update_depart.html', {'depart': depart})
